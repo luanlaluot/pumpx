b11cbefe570cce9ae6513623846ff7a1d5798493
luan na <luan.na@urbox.vn>
2024-11-23 15:19:24 +0700

    update log

--- Applications/pumpx-be/src/main.ts
+++ Applications/pumpx-be/src/main.ts
@@ -29,8 +29,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('API running on http://localhost:3000');
-  console.log('Swagger Docs available at http://localhost:3000/api-docs');
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`API running on http://localhost:${port}`);
+  console.log(`Swagger Docs available at http://localhost:3000/${port}`);
 }
 bootstrap();
